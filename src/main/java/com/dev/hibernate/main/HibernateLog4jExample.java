d5559beab6e633b5d12a0aca04dd6b38444d585d
Anuj Kumar Sharma <anujkumar.sharma@ellucian.com>
2019-06-17 18:45:38 +0530

    Log4j added to Hibernate

---
+++ src/main/java/com/dev/hibernate/main/HibernateLog4jExample.java
@@ -0,0 +1,43 @@
+package com.dev.hibernate.main;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.dev.hibernate.model.Person;
+import com.dev.hibernate.util.HibernateUtil;
+
+public class HibernateLog4jExample {
+	static{
+		System.out.println("Before log4j configuration");
+		DOMConfigurator.configure("log4j.xml");
+		System.out.println("After log4j configuration");
+	}
+	
+	private static Logger logger = Logger.getLogger(HibernateLog4jExample.class);
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		
+		//Prep work
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+		//Get All Employees
+		Transaction tx = session.beginTransaction();
+		Query query = session.createQuery("from Person");
+		List<Person> empList = query.list();
+		for(Person emp : empList){
+			logger.info("List of Employees::"+emp.getId()+","+emp.getAddress().getCity());
+		}
+		
+		tx.commit();
+		sessionFactory.close();
+		logger.info("DONE");
+	}
+
+}
